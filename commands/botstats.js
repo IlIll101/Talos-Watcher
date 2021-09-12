4fb8549bde5a532a916bb96b8f4618627adb6ccc
IlIll101 <80976955+IlIll101@users.noreply.github.com>
2021-09-12 18:06:46 -0400

    Update botstats.js

--- commands/botstats.js
+++ commands/botstats.js
@@ -8,20 +8,29 @@ module.exports = {
 		.setDescription('displays information about the bot'),
     
     async execute(interaction, client) {
-        function mil_to_HM (timeInt) {
-            let time = Math.round((timeInt/3600000)*100).toString();
-            if (time.lenght > 3) {
-                return (`${time[0] + time[1]} Hours, ${time[2] + time[3]} Minutes`);
+        function mil_to_HM (t) {
+            var cd = 24 * 60 * 60 * 1000,
+            ch = 60 * 60 * 1000,
+            d = Math.floor(t / cd),
+            h = Math.floor( (t - d * cd) / ch),
+            m = Math.round( (t - d * cd - h * ch) / 60000),
+            pad = function(n){ return n < 10 ? '0' + n : n; };
+            if( m === 60 ){
+                h++;
+                m = 0;
             }
-            else {
-                return (`0${time[0]} Hours, ${time[1] + time[2]} Minutes`);
+            if( h === 24 ){
+                d++;
+                h = 0;
             }
+            return [d, pad(h), pad(m)].join(':');
         }
+
         const embed = new MessageEmbed()
         .setColor('BLUE')
         .setTitle(`Stats about: ${client.user.tag}`)
         .addFields(
-            { name: 'Uptime', value: mil_to_HM(client.uptime), inline: false },
+            { name: 'Uptime', value: mil_to_HM(client.uptime).toString(), inline: false },
             { name: 'Servers present', value: client.guilds.cache.size.toString(), inline: false },
             { name: 'Bot ping', valie: `${client.ws.ping} ms`, inline: false },
             { name: 'Memory usage', value: `${Math.round(os.totalmem() - os.freemem()/10000)}/${Math.round(os.totalmem()/10000)} mbs in use`, inline: false },
